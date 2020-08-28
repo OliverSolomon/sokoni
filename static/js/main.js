c1c4b8351838940a30bcd3d5fb24a7174a8c987c
oliver <oliv3r@pop-os.localdomain>
2020-08-28 15:17:34 +0300

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+// alert("Waddup Fuckers");
+
+var elem = document.getElementById('top');
+var colorGot = window.getComputedStyle(elem, null).getPropertyValue("background-color");
+
+//???????????????? Seting a changing background color
+var colorGot2 = "Red";
+
+// alert(colorGot);
+// document.getElementById("top").style.backgroundColor = colorGot2;
+
+
+
+//changing slides
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 33.3 minutes
+}
+
